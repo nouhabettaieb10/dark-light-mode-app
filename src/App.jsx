44092b007250a34f6b985ae6fea62f285b6f7f73
nouhabettaieb10 <nouhabettaieb92@gmail.com>
2026-06-21 15:52:13 +0200

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/about" element={<About />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/overview" element={<Overview />} />
         </Routes>
       </BrowserRouter>
     </div>
